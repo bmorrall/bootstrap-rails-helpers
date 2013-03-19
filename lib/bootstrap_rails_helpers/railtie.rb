940971e784a3bb1f3725ea00063f58d4c606c68b
bmorrall <ben.morrall@sitepoint.com>
2013-03-19 16:23:21 +1100

    Added Flash Helper

--- lib/bootstrap_rails_helpers/railtie.rb
+++ lib/bootstrap_rails_helpers/railtie.rb
@@ -1,9 +1,11 @@
 require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/breadcrumbs_helper'
+require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/flash_helper'
 
 module BootstrapRailsHelpers
   class Railtie < Rails::Railtie
     initializer "bootstrap_rails_helpers.breadcrumbs_helper" do
       ActionView::Base.send :include, BootstrapRailsHelpers::BreadcrumbsHelper
+      ActionView::Base.send :include, BootstrapRailsHelpers::FlashHelper
       ActionController::Base.append_view_path File.dirname(__FILE__) + "/../../app/views"
     end
   end
---
+++ spec/helpers/bootstrap_rails_helpers/flash_helper_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe BootstrapRailsHelpers::FlashHelper do
+  pending "write it"
+end
