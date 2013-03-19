5e3bcfab3458f75b275abb4357c5f68214d41e03
bmorrall <ben.morrall@sitepoint.com>
2013-03-19 16:57:35 +1100

    Fixed indentation and module namespacing

--- app/helpers/bootstrap_rails_helpers/breadcrumbs_helper.rb
+++ app/helpers/bootstrap_rails_helpers/breadcrumbs_helper.rb
@@ -1,25 +1,23 @@
-module BootstrapRailsHelpers
-  module BreadcrumbsHelper
+module BootstrapRailsHelpers::BreadcrumbsHelper
 
-    def add_breadcrumb(name, url = '', options = {})
-      @breadcrumbs ||= []
-      name = translate_breadcrumb(name, params[:controller]) if name.is_a?(Symbol)
-      url = eval(url.to_s) if url =~ /_path|_url|@/
-      @breadcrumbs << {:name => name, :url => url, :options => options}
-    end
-
-    def translate_breadcrumb(name, controller_path)
-      scope = [:breadcrumbs]
-      namespace = controller_path.split('/')
-      namespace.last.sub!('_controller', '')
-      scope += namespace
-
-      I18n.t name, :scope => scope, :default => I18n.t("breadcrumbs.defaults.#{name}")
-    end
-
-    def render_breadcrumbs(divider = '/')
-      render :partial => 'bootstrap_rails_helpers/breadcrumbs', :locals => {:divider => divider}
-    end
-
+  def add_breadcrumb(name, url = '', options = {})
+    @breadcrumbs ||= []
+    name = translate_breadcrumb(name, params[:controller]) if name.is_a?(Symbol)
+    url = eval(url.to_s) if url =~ /_path|_url|@/
+    @breadcrumbs << {:name => name, :url => url, :options => options}
   end
+
+  def translate_breadcrumb(name, controller_path)
+    scope = [:breadcrumbs]
+    namespace = controller_path.split('/')
+    namespace.last.sub!('_controller', '')
+    scope += namespace
+
+    I18n.t name, :scope => scope, :default => I18n.t("breadcrumbs.defaults.#{name}")
+  end
+
+  def render_breadcrumbs(divider = '/')
+    render :partial => 'bootstrap_rails_helpers/breadcrumbs', :locals => {:divider => divider}
+  end
+
 end
