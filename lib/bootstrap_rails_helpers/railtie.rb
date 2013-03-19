22cdcf601529dc75d4157f9fd1013cc414cc717a
bmorrall <ben.morrall@sitepoint.com>
2013-03-19 17:49:57 +1100

    Added Modal Helper

--- lib/bootstrap_rails_helpers/railtie.rb
+++ lib/bootstrap_rails_helpers/railtie.rb
@@ -1,11 +1,13 @@
 require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/breadcrumbs_helper'
 require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/flash_helper'
+require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/modal_helper'
 
 module BootstrapRailsHelpers
   class Railtie < Rails::Railtie
     initializer "bootstrap_rails_helpers.breadcrumbs_helper" do
       ActionView::Base.send :include, BootstrapRailsHelpers::BreadcrumbsHelper
       ActionView::Base.send :include, BootstrapRailsHelpers::FlashHelper
+      ActionView::Base.send :include, BootstrapRailsHelpers::ModalHelper
       ActionController::Base.append_view_path File.dirname(__FILE__) + "/../../app/views"
     end
   end
---
+++ spec/helpers/bootstrap_rails_helpers/modal_helper_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe BootstrapRailsHelpers::ModalHelper do
+  pending "write it"
+end
