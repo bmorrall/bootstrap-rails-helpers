7be41000bc6c453495b342e1bda6174bf5044593
bmorrall <ben.morrall@sitepoint.com>
2013-03-19 17:53:57 +1100

    Added Glyph Helper

--- lib/bootstrap_rails_helpers/railtie.rb
+++ lib/bootstrap_rails_helpers/railtie.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/breadcrumbs_helper'
 require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/flash_helper'
+require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/glyph_helper'
 require File.dirname(__FILE__) + '/../../app/helpers/bootstrap_rails_helpers/modal_helper'
 
 module BootstrapRailsHelpers
@@ -7,6 +8,7 @@ module BootstrapRailsHelpers
     initializer "bootstrap_rails_helpers.breadcrumbs_helper" do
       ActionView::Base.send :include, BootstrapRailsHelpers::BreadcrumbsHelper
       ActionView::Base.send :include, BootstrapRailsHelpers::FlashHelper
+      ActionView::Base.send :include, BootstrapRailsHelpers::GlyphHelper
       ActionView::Base.send :include, BootstrapRailsHelpers::ModalHelper
       ActionController::Base.append_view_path File.dirname(__FILE__) + "/../../app/views"
     end
---
+++ spec/helpers/bootstrap_rails_helpers/glyph_helper_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe BootstrapRailsHelpers::GlyphHelper do
+  pending "write it"
+end
