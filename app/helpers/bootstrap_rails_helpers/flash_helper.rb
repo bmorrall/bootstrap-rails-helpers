01c8111af0bc7ece7faf619e1c0c2a2fee1008e5
bmorrall <ben.morrall@sitepoint.com>
2013-03-19 16:49:39 +1100

    Added flash/alert message helpers

--- app/helpers/bootstrap_rails_helpers/flash_helper.rb
+++ app/helpers/bootstrap_rails_helpers/flash_helper.rb
@@ -2,6 +2,13 @@ module BootstrapRailsHelpers
   module FlashHelper
     ALERT_TYPES = [:error, :info, :success, :warning]
 
+    def alert_message(message, options = {})
+      render :partial => 'bootstrap_rails_helpers/flash_alert', :locals => {
+        :alert_type => options[:alert_type],
+        :message => message
+      }
+    end
+
     def bootstrap_flash
       flash_messages = []
       flash.each do |type, message|
@@ -13,9 +20,7 @@ module BootstrapRailsHelpers
         next unless ALERT_TYPES.include?(type)
 
         Array(message).each do |msg|
-          text = content_tag(:div,
-            content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-            msg.html_safe, :class => "alert fade in alert-#{type}")
+          text = alert_message(msg, :alert_type => type)
           flash_messages << text if message
         end
       end
