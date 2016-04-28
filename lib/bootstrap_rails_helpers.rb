8e9174df9b0eaaf8a9f7d36a769a54081322e3c0
bastengao <bastengao@gmail.com>
2016-04-28 17:55:42 +0800

    add bootstrap version configuration

--- lib/bootstrap_rails_helpers.rb
+++ lib/bootstrap_rails_helpers.rb
@@ -1,7 +1,12 @@
 require "bootstrap_rails_helpers/version"
 
 module BootstrapRailsHelpers
-  # Your code goes here...
+  mattr_accessor :bootstrap_version
+  @@bootstrap_version = 3
+
+  def self.setup
+    yield self
+  end
 end
 
 require 'bootstrap_rails_helpers/railtie' if defined?(Rails)
